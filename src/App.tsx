f116440d1458b9645335d35e33956c488ac7a233
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2025-03-08 04:56:36 -0300

    pokemons page getting started

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import axios from 'axios'
 import './App.css'
+import PokemonCard from './components/PokemonCard'
 
 function App() {
+
+  function getPokeApi() {
+    try {
+      axios.get("https://pokeapi.co/api/v2/pokemon?limit=21")
+      .then((res) => console.log(res.data.results))
+      .catch((err) => console.log(err))
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <>
+      {getPokeApi()}
       <h1>Página de pokemons!</h1>
+      <PokemonCard />
     </>
   )
 }
---
+++ src/components/PokemonCard/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./PokemonCard.module.css"
+
+function PokemonCard() {
+    return (
+        <div>
+            <div className={styles.container}>
+                <p>pokemon card</p>
+            </div>
+        </div>
+    )
+}
+
+export default PokemonCard
